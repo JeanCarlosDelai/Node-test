30ac3eec6e623eb969aeca4eb4316e5fbc67c988
Jean Carlos Delai <112594276+JeanCarlosDelai@users.noreply.github.com>
2024-06-21 16:27:02 -0300

    feat: Add UserRepositoryInterface

---
+++ src/modules/domain/interfaces/UserRepository.interfece.ts
@@ -0,0 +1,11 @@
+import { CreateUserDTO } from "../dtos/CreateUser.dto";
+import { IUser } from "./User.interface";
+
+export interface IUsersRepository {
+  findAll(): Promise<any>;
+  findByName(name: string): Promise<IUser | null>;
+  findById(id: string): Promise<IUser | null>;
+  findByEmail(email: string): Promise<IUser | null>; 
+  create(createUserDTO: CreateUserDTO): Promise<IUser>;
+  save(user: IUser): Promise<IUser>;
+}
