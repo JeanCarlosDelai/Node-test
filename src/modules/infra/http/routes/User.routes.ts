c91a625f289936b349aebb7bac1b1be9ddc9715b
Jean Carlos Delai <112594276+JeanCarlosDelai@users.noreply.github.com>
2024-06-21 17:18:37 -0300

    feat: Add validator to routes

--- src/modules/infra/http/routes/User.routes.ts
+++ src/modules/infra/http/routes/User.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import CreateUserController from '../controllers/CreateUserController';
+import Validator from 'src/shared/infra/http/middlewares/Validator';
+import { CreateUserSchema } from '../schemas/CreateUserSchema';
 
 const usersRouter = Router();
 const createUserController = new CreateUserController();
 
-usersRouter.post('/user', createUserController.execute);
+usersRouter.post('/user', Validator(CreateUserSchema), createUserController.execute);
 
 export default usersRouter;
