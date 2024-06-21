9feb2181da330f79b71a8d464b184504ebf129c0
Jean Carlos Delai <112594276+JeanCarlosDelai@users.noreply.github.com>
2024-06-21 17:18:57 -0300

    refactor: more updates

--- src/modules/useCases/CreateUserUseCase.ts
+++ src/modules/useCases/CreateUserUseCase.ts
@@ -1,11 +1,10 @@
 import { inject, injectable } from 'tsyringe';
 import { IUsersRepository } from '../domain/interfaces/UserRepository.interfece';
 import { BadRequestError } from 'src/shared/errors/BadRequest.error';
-import { EMAIL_ALREADY_USED } from 'src/shared/consts/ErrorConsts';
 import { CreateUserDTO } from '../domain/dtos/CreateUser.dto';
 import UserResponseMapper from '../domain/mappings/UserResponseMapper';
 import { UserRespondeDTO } from '../domain/dtos/UserReponse.dto';
-import { UsersRepository } from '../infra/typeorm/repositories/implementation/UsersRepository';
+import { EMAIL_ALREADY_USED } from 'src/shared/consts/ErrorMessagesConsts';
 
 @injectable()
 export class CreateUserUseCase {
@@ -19,7 +18,7 @@ export class CreateUserUseCase {
     const emailExists = await this.usersRepository.findByEmail(createUserDTO.email);
 
     if (emailExists) {
-      throw new BadRequestError(EMAIL_ALREADY_USED);
+      throw new BadRequestError([EMAIL_ALREADY_USED]);
     }
     const newUser =  await this.usersRepository.create(createUserDTO);
     const response =  await this.usersRepository.save(newUser);
