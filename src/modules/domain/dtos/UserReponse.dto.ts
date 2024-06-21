5b3148678863a70ce6f81666dc4f3a377228d866
Jean Carlos Delai <112594276+JeanCarlosDelai@users.noreply.github.com>
2024-06-21 16:27:51 -0300

    feat: Add userResponse.dto

---
+++ src/modules/domain/dtos/UserReponse.dto.ts
@@ -0,0 +1,9 @@
+export class UserRespondeDTO {
+    readonly id: string;
+    readonly name: string;
+    readonly email: string;
+  
+    constructor(id: string, name: string, email:string) {
+      Object.assign(this, { id, name, email });
+    }
+  }
