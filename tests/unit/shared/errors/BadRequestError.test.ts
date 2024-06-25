fc54951e72b90c70b418df7ff56cc82458b530e4
Jean Carlos Delai <112594276+JeanCarlosDelai@users.noreply.github.com>
2024-06-25 14:27:36 -0300

    feat: Add BadRequest.test

---
+++ tests/unit/shared/errors/BadRequestError.test.ts
@@ -0,0 +1,11 @@
+import { BadRequestError } from "src/shared/errors/BadRequestError";
+
+describe('BadRequestError', () => {
+    it('should create an instance of BadRequestError correctly', () => {
+        const message = 'Invalid request error';
+        const badRequestError = new BadRequestError(message);
+
+        expect(badRequestError.message).toBe(message);
+        expect(badRequestError.statusCode).toBe(400);
+    });
+});
