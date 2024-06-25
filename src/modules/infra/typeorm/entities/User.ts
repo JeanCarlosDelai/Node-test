4c57a30c066f70d338559b607993abd2feb9cbcb
Jean Carlos Delai <112594276+JeanCarlosDelai@users.noreply.github.com>
2024-06-25 19:05:13 -0300

    remove: remove exclude

--- src/modules/infra/typeorm/entities/User.ts
+++ src/modules/infra/typeorm/entities/User.ts
@@ -1,34 +1,31 @@
 import {
-    Column,
-    CreateDateColumn,
-    Entity,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn,
-  } from 'typeorm';
-  import { Exclude } from 'class-transformer';
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { IUser } from 'src/modules/domain/interfaces/User.interface';
-  
-  @Entity('users')
-  class User implements IUser {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
-  
-    @Column()
-    name: string;
-  
-    @Column()
-    email: string;
-  
-    @Column()
-    @Exclude()
-    password: string;
-  
-    @CreateDateColumn()
-    created_at: Date;
-  
-    @UpdateDateColumn()
-    updated_at: Date;
 
-  }
-  
-  export default User;
+@Entity('users')
+class User implements IUser {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
